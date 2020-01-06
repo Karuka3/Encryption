f5a08e4a37627450d90d8dc911177992ff49054d
Karuka3 <karuka.piplup.0327@gmail.com>
2020-01-06 18:09:40 +0900

    スキュタレー暗号の実装

--- encryption.py
+++ encryption.py
@@ -1,6 +1,10 @@
+import random
+import string
+
+
 class Caesar:
-    def __init__(self, plain="DeMO", key=3):
-        self.plain = plain
+    def __init__(self, text="DeMO", key=3):
+        self.text = text
         self.key = self.keygen(key)
 
     def keygen(self, key):
@@ -8,7 +12,8 @@ class Caesar:
 
     def encording(self):
         cipher = ""
-        for char in self.plain:
+        plain = self.text
+        for char in plain:
             ASCII = ord(char)
             if ord('A') <= ASCII <= ord('Z'):
                 cipher += chr((ASCII - ord('A') - self.key) % 26 + ord('A'))
@@ -18,11 +23,12 @@ class Caesar:
                 cipher += chr((ASCII - ord('0') - self.key) % 10 + ord('0'))
             else:
                 cipher += char
-        print("暗号文: {}".format(cipher))
+        print("Cryotigram: {}".format(cipher))
         return cipher
 
-    def decording(self, cipher):
+    def decording(self):
         plain = ""
+        cipher = self.text
         for char in cipher:
             ASCII = ord(char)
             if ord('A') <= ASCII <= ord('Z'):
@@ -35,8 +41,61 @@ class Caesar:
                 plain += char
         return plain
 
-    def attack(self, cipher):
+    def attack(self):
         for key in range(26):
             self.key = key
-            plain = self.decording(cipher)
+            plain = self.decording()
             print("Key:{}".format(key) + "\n" + plain)
+
+
+class Skytale:
+    def __init__(self, text="This is a test encryption", key=3):
+        self.text = text.replace(' ', '')
+        self.key = self.keygen(key)
+
+    def keygen(self, key):
+        return key
+
+    def encording(self):
+        cipher = ""
+        plain = self.text
+        mod = len(plain) % self.key
+        block = -(-len(plain) // self.key) + (1 if mod != 0 else 0)
+        for j in range(self.key):
+            for k in range(block):
+                index = k*self.key + j
+                if index < len(plain):
+                    cipher += plain[index]
+                else:
+                    cipher += random.choice(string.ascii_letters)
+        print("Cryotigram: {}".format(cipher))
+        return cipher
+
+    def decording(self):
+        plain = ""
+        chiper = self.text
+        for j in range(self.key):
+            for k in range(j, len(chiper), self.key):
+                plain += chiper[k]
+        return plain
+
+    def attack(self):
+        wordcount = len(self.text)
+        divisor = [i for i in range(2, wordcount) if wordcount % i == 0]
+        print("List of potential keys:{}".format(divisor))
+        if divisor:
+            for key in divisor:
+                self.key = key
+                plain = self.decording()
+                print("Key:{}".format(self.key) + "\n" + plain)
+        else:
+            print("This text may not be Skytale encryption, because the number of characters in the text is a prime number.")
+
+
+def test():
+    s = Skytale(text="HolheWdoloXBlrnp", key=4)
+    s.attack()
+
+
+if __name__ == "__main__":
+    test()
